1070615ff8911afc1d672f2f6e3d49cc08fe7463
Welington FIdelis <welington.fidelis@tech4h.com.br>
2020-12-06 20:28:29 -0300

    Salas de chat 1.0

--- src/server.js
+++ src/server.js
@@ -5,10 +5,17 @@ const io = require('socket.io')(server, {cors: { origins:'*' }});
 const cors = require('cors');
 const port = 3001;
 const connectedUsers = {}
+const activeRooms = {
+    sala1: { message: [], connectedUsers: {}, total: 0 },
+    sala2: { message: [], connectedUsers: {}, total: 0 }
+}
 
 app.use(cors());
+
 io.on('connection', (client) => {
+    console.log('CONETANDO', client.handshake.query);
     client.emit('receiveListOnlineUsers', connectedUsers);
+    client.emit('receiveListRooms', activeRooms);
 
     const { user } = client.handshake.query;
     const newUser = { user, socketId: client.id, message: [] }
@@ -21,10 +28,40 @@ io.on('connection', (client) => {
         io.to(receiver).emit("receiveMessage", { message, sender: client.id });
     });
 
+    client.on('sendMessageToRoom', data => {
+        const { room, message } = data;
+
+        client.to(room).emit(
+            "receiveMessageFromRoom", 
+            { room, message: {...message, socketId: client.id} }
+        );
+    });
+
+    client.on('joinRoom', data => {
+        const { room, user } = data;
+
+        activeRooms[room].connectedUsers[client.id] = true;
+        activeRooms[room].total += 1;
+
+        client.join(room);
+        io.emit('newUserInRoom', { room, user });
+    });
+
     client.on('disconnect', data => {
         delete connectedUsers[client.id]
 
         client.broadcast.emit('removeOnlineUser', client.id);
+
+        for(const room of Object.entries(activeRooms)) {
+            const [ key, value ] = room;
+            const { connectedUsers } = value;
+            if(connectedUsers[client.id]) {
+                delete activeRooms[key].connectedUsers[client.id];
+                activeRooms[key].total -= 1;
+
+                io.emit('removeUserRoom', { room: key });
+            }
+        }
     });
 });
 
